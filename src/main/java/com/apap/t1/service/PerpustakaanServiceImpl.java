ad4f875b9241dea9ef052c3e09a32d689b645b04
Annisa Avianti <annisa.avianti@ui.ac.id>
2020-03-23 19:09:07 +0700

    PerpustakaanServiceImpl added

---
+++ src/main/java/com/apap/t1/service/PerpustakaanServiceImpl.java
@@ -0,0 +1,44 @@
+package com.apap.t1.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.apap.t1.model.PerpustakaanModel;
+import com.apap.t1.repository.PerpustakaanDb;
+
+/**
+ * PerpustakaanServiceImpl
+ */
+@Service
+@Transactional
+public class PerpustakaanServiceImpl implements PerpustakaanService{
+
+	@Autowired
+	private PerpustakaanDb perpustakaanDb;
+	
+	@Override
+	public List<PerpustakaanModel> getPerpustakaanList() {
+		return perpustakaanDb.findAll();
+	}
+
+	@Override
+	public PerpustakaanModel getPerpustakaanById(int id) {
+		return perpustakaanDb.findById(id);
+	}
+
+	@Override
+	public void addPerpustakaan(PerpustakaanModel perpustakaan) {
+		perpustakaanDb.save(perpustakaan);
+	}
+
+	@Override
+	public void deletePerpustakaan(int id) {
+		perpustakaanDb.delete(this.getPerpustakaanById(id));
+	}
+	
+	
+
+}
